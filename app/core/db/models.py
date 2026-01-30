066b1a0053f764b0400efc99d0cde7327488dba5
incaholy <max.nunez@live.com>
2026-01-30 13:54:57 -0800

    added a base model for all future units

--- app/core/db/models.py
+++ app/core/db/models.py
@@ -5,9 +5,25 @@ from sqlalchemy import Column, CheckConstraint, String, Text
 from sqlalchemy.dialects.postgresql import JSONB
 
 
-class Unit(SQLModel, table = True):
-    __tablename__ = "Units"
-    __table_args__ = (
-        CheckConstraint("char_length(trim(destination_username)) > 0", name="ck_messages_destination_username_nonempty"),
-        CheckConstraint("char_length(trim(subject_text)) > 0", name="ck_messages_subject_text_nonempty"),
-    )
+class Unit(SQLModel):
+    name: str
+    faction: str
+    movement: int
+    toughness: int
+    save: int
+    wounds: int
+    leadership_value: int
+    objective_control: int
+    weapons: list[str]
+    abilities: list[str]
+    keywords: list[str]
+
+
+
+class Infantry(Unit):
+    __tablename__ = "infantry"
+
+
+
+class Vehicle(Unit):
+    __tablename__ = "vehicles"
