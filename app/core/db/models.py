c9a8f8dda9ec285005ab43f159fa5ecd249f767a
incaholy <max.nunez@live.com>
2026-01-29 18:57:18 -0800

    made empty table for unit

--- app/core/db/models.py
+++ app/core/db/models.py
@@ -3,3 +3,11 @@ from typing import Any, Dict, List, Optional
 from sqlmodel import Field, Relationship, SQLModel
 from sqlalchemy import Column, CheckConstraint, String, Text
 from sqlalchemy.dialects.postgresql import JSONB
+
+
+class Unit(SQLModel, table = True):
+    __tablename__ = "Units"
+    __table_args__ = (
+        CheckConstraint("char_length(trim(destination_username)) > 0", name="ck_messages_destination_username_nonempty"),
+        CheckConstraint("char_length(trim(subject_text)) > 0", name="ck_messages_subject_text_nonempty"),
+    )
